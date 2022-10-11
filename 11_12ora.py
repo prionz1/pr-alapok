a1b301c7efd97d553f97eca48fe838e06ce874b9
prionz1 <112930799+prionz1@users.noreply.github.com>
2022-10-11 09:40:54 +0200

    Create 11_12ora.py

---
+++ 11_12ora.py
@@ -0,0 +1,37 @@
+'''gondolt_szám = 4 #feladat 1
+tipp = input('Gondoltam gey számra. Tippeld meg! ')
+tipp = int(tipp)
+if tipp == gondolt_szám:
+    print('Ügyes!')
+print('pápá')'
+
+gondolt_szám = 4 
+tipp = input('Gondoltam gey számra. Tippeld meg! ')
+tipp = int(tipp)
+
+if tipp == gondolt_szám:
+    print('Ügyes! ')
+    print('Gratulálok! ')
+else:
+    print('Hosszan gondolköodtál rajta?:)')
+    print('Nem érte meg')
+print('Pápá')'''
+
+
+
+'''szám1 = input('kérek egy számot' )
+szám1 = int(szám1)
+szám2 = input('kérek egy másik számot' )
+szám2 = int(szám2)
+
+if szám1 == szám2:
+    print('a két szám meg eggyezik' )
+if szám1 =! szám2:
+    print('a két szám nem eggyezik meg' )
+print('program vége' )'''
+
+if szám1 < szám2:
+    print('az első szám kisebb')
+else:
+    print('az első szám nagyobb vagy egyenlő')
+print('program vége' )
