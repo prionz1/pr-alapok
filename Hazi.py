c3f24df560eb3576febe321e014e13194116df71
prionz1 <112930799+prionz1@users.noreply.github.com>
2022-10-11 07:38:48 +0200

    Create Hazi.py

---
+++ Hazi.py
@@ -0,0 +1,16 @@
+szám1 = input('irj be egy számot: ')
+szám2 = input('irj be még egy-et: ')
+
+
+'''sum = float(szám1) + float(szám2)
+print(szám1, 'meg', szám2 , 'annyi mint' ,sum)
+sum = float(szám1) - float(szám2)
+print(szám1, 'minusz', szám2 , 'annyi mint' ,sum)
+sum = float(szám1) * float(szám2)
+print(szám1, 'szorozva', szám2 ,'el/al' , 'annyi mint' ,sum)'''
+
+sum = float(szám1) / float(szám2)
+
+
+print(szám1, 'osztva', szám2 ,'vel/al/el' , 'annyi mint' ,sum)
+
