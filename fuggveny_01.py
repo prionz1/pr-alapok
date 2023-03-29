6598b0bd7db934c3a8f28d37807b9e1cad294439
prionz1 <112930799+prionz1@users.noreply.github.com>
2023-03-29 08:36:55 +0200

    Create fuggveny_01.py

---
+++ fuggveny_01.py
@@ -0,0 +1,10 @@
+#chr() A chr() függvény azt a karaktert adja vissza, amely a megadott unikódot képviseli.
+#ord() Az ord() függvény a megadott karakter unicode kódját jelentő számot adja vissza.
+
+lista = [1,2,3,4,5,6,7,8,9,10,11,12,12,13,14]
+
+print(min(lista))
+
+print(max(lista))
+
+print(lista[5])
